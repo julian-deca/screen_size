5e8f004509890d02c7b0ebbafd1c7c91830e09bd
julian deca <juliandecastelli@gmail.com>
2022-11-01 13:11:14 -0300

    fix png size

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-canvas.width = 480;
-canvas.height = 320;
+canvas.width = 640;
+canvas.height = 360;
 
 const image = background;
 ctx.drawImage(image, 0, 0);
