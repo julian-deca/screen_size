066044cdee9d3d4d6295c872ac743962e1b44720
julian deca <juliandecastelli@gmail.com>
2022-11-01 19:24:17 -0300

    add fullScreenButton

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+const fullScreenButton = document.getElementById("fullScreenButton");
 canvas.width = 1920;
 canvas.height = 1080;
 
 const image = background;
 ctx.drawImage(image, 0, 0);
+function toggleFullScreen() {
+  if (!document.fullscreenElement) {
+    canvas.requestFullscreen().catch((err) => {
+      alert("Error");
+    });
+  } else {
+    document.exitFullscreen();
+  }
+}
+fullScreenButton.addEventListener("click", toggleFullScreen);
