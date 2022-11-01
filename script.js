7836c60509d8743b98e8e333f9336c538e4d5fd8
julian deca <juliandecastelli@gmail.com>
2022-11-01 16:38:51 -0300

    changed png size

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
-canvas.width = 640;
-canvas.height = 360;
+canvas.width = 1920;
+canvas.height = 1080;
 
 const image = background;
 ctx.drawImage(image, 0, 0);
